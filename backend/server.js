eee7bb297482ed44c6d4ea5f4315a35996cc2e8d
mohmdhozaifa <hozaifazamir421@gmail.com>
2025-10-30 01:28:17 +0530

    allow all

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const  PORT =  process.env.PORT || 3000
 connectDb()
 app.use(express.json())
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
+    origin:true,
     credentials: true,
 }))
 app.use(cookieParser());
