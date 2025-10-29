4d2821dea47e5dfb637f7702c0fc3f3f2751293f
mohmdhozaifa <hozaifazamir421@gmail.com>
2025-10-30 00:02:20 +0530

    frontend deploy try

--- frontend/snippets/src/api/axios.js
+++ frontend/snippets/src/api/axios.js
@@ -8,7 +8,7 @@ export const injectStore = (_store) => {
 };
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
